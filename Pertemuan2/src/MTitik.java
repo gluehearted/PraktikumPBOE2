f14fcadd73e06563f3ac9490ee1b8b62317a023e
Gabriel Prakosa A <gabriel18prakosa@gmail.com>
2026-02-26 21:06:19 +0700

    pertemuan2

---
+++ Pertemuan2/src/MTitik.java
@@ -0,0 +1,23 @@
+/* Nama File    : MTitik.java
+ * Deskripsi    : Main class untuk menjalankan class Titik
+ * Pembuat      : 
+ * Tanggal      : 
+ */
+
+public class MTitik {
+    public static void main(String[] args) {
+        Titik T1 = new Titik(); // Membuat objek titik T1 (0,0)
+        
+        T1.setAbsis(3);         // Mengubah absis T1 dengan nilai 3
+        T1.setOrdinat(4);       // Mengubah ordinat T1 dengan nilai 4
+        T1.printTitik();        // Mencetak koordinat T1 ke layar
+        T1.geser(3, 4);         // Menggeser T1 sejauh (3,4)
+        T1.printTitik();        // Menampilkan koordinat T1 setelah digeser
+
+        Titik T2 = T1;
+        T2.printTitik();
+        T1.setAbsis(10);
+        T1.setOrdinat(10);
+        T2.printTitik();
+    }
+}
